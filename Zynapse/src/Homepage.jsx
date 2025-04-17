a583d858aa8391a7d210bf4357fc867f57965835
Archit Shukla <asharchit03@gmail.com>
2025-04-18 03:17:44 +0530

    Added landing Page For the webiste

---
+++ Zynapse/src/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BrainLoadEffect from "./components/BrainLoadEffect";
+import InfoBoard from "./components/InfoBoard";
+
+const Homepage = () => {
+  return (
+    <>
+      <InfoBoard />
+    </>
+  );
+};
+
+export default Homepage;
