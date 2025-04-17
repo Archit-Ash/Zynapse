154d5222b22821022c4b4f21bdd7297b40e7fa11
Archit Shukla <asharchit03@gmail.com>
2025-04-18 01:31:58 +0530

    Added Background Animation and effects for Website

--- Zynapse/src/main.jsx
+++ Zynapse/src/main.jsx
@@ -1,5 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import BrainLoadEffect from "./components/BrainLoadEffect";
 
-createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrainLoadEffect />
+  </StrictMode>
+);
