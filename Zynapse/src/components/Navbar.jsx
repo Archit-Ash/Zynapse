6f094601eef0685b19f8343bb7b807931e5ab13b
Archit Shukla <asharchit03@gmail.com>
2025-04-25 06:31:26 +0530

    AI BASED Questionaire implemented , Beautification Pending

--- Zynapse/src/components/Navbar.jsx
+++ Zynapse/src/components/Navbar.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logoZynapse from "../assets/zynapse Logo.png";
 import "../style/NavbarStyle.css";
+import { useLocation } from "react-router-dom";
 
+const pageNames = {
+  "/": "Home",
+  "/upload": "Image Upload",
+};
 const Navbar = () => {
+  const location = useLocation();
+  const pageName = pageNames[location.pathname] || "Page";
+
   return (
     <>
       <div id="navbar-flex-div">
@@ -12,9 +20,12 @@ const Navbar = () => {
             <img id="logoNavbar" src={logoZynapse} />
           </div>
         </Link>
-        <div>
-          <img id="logoNavbar" src={logoZynapse} />
-        </div>
+        <div id="pagename">{pageName}</div>
+        <Link to="/question">
+          <div>
+            <img id="logoNavbar" src={logoZynapse} />
+          </div>
+        </Link>
       </div>
     </>
   );
